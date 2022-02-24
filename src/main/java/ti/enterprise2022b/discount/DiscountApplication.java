10b2bbaeaf9160a09eedad8ca8add3e2ebd3a42c
Abdil Tegar Arifin <abdiltegar12@gmail.com>
2022-02-24 10:02:10 +0700

    First Push

---
+++ src/main/java/ti/enterprise2022b/discount/DiscountApplication.java
@@ -0,0 +1,13 @@
+package ti.enterprise2022b.discount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountApplication.class, args);
+	}
+
+}
